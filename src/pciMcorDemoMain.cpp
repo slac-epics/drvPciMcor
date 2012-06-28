ac9080bfbe938417f9f3c54fa6efba14ba98d1d4
strauman <>
2012-06-28 19:50:02 +0000

    - use CEXP if available

--- src/pciMcorDemoMain.cpp
+++ src/pciMcorDemoMain.cpp
@@ -2,13 +2,21 @@
 #include <epicsThread.h>
 #include <epicsExit.h>
 
+#ifdef HAVE_CEXP
+#include <cexp.h>
+#endif
+
 int
 main(int argc, char **argv)
 {
+#ifdef HAVE_CEXP
+	cexp_main(argc, argv);
+#else
 	if ( argc >= 2 ) {
 		iocsh(argv[1]);
 		epicsThreadSleep(.2);
 	}
 	iocsh(NULL);
+#endif
 	epicsExit(0);
 }
