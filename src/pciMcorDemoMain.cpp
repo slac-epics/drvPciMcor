1d0ee82795797fc8640b474b30f8b9f02ad74ca4
strauman <>
2012-10-05 17:27:59 +0000

    - added script for starting up cexp demo  - added 'md' utility for memory/register dumps

--- src/pciMcorDemoMain.cpp
+++ src/pciMcorDemoMain.cpp
@@ -2,9 +2,69 @@
 #include <epicsThread.h>
 #include <epicsExit.h>
 
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #ifdef HAVE_CEXP
 #include <cexp.h>
 #endif
+
+#define PREF ""
+
+static void dumpchars(unsigned char **pstr, unsigned char *end)
+{
+    printf("  ");
+    while ( *pstr < end ) {
+        fputc(isprint(**pstr) ? **pstr : '.', stdout);
+        (*pstr)++;
+    }
+}
+
+static void dumpval(unsigned address, int size)
+{
+    switch(abs(size)) {
+        case 4: printf(PREF"%08x", *(unsigned*)address);        break;
+        case 2: printf(PREF"%04x", *(unsigned short*)address);  break;
+        case 1: printf(PREF"%02x", *(unsigned char *)address);  break;
+        default:
+        break;
+    }
+}
+
+
+extern "C" int
+md(unsigned address, int count, int size)
+{
+int i = 0;
+unsigned char *oadd = (unsigned char*)address;
+	switch (size) {
+		default:	size=4;
+		case 1: case 2: case 4:
+		break;
+	}
+	while ( i < count) {
+		if ( i%16 == 0 ) {
+			if (i) {
+				dumpchars(&oadd, (unsigned char*)address);
+			}
+			printf("\n0x%08x:", address);
+		}
+		printf("  ");
+		dumpval(address,size);
+		i += size;
+		address += size;
+	}
+
+	if (count > 0) {
+		printf("%*s",(((i+15)/16)*16 - i)/size * (2 + (int)strlen(PREF) + 2*size),"");
+		dumpchars(&oadd,(unsigned char*)address);
+	}
+	fputc('\n',stdout);
+	return 0;
+}
+
 
 int
 main(int argc, char **argv)
